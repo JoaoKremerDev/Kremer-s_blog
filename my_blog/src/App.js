ff1f446a96c4a7027b5811c026009dab157d1807
João Vitor Melo Kremer <joaokremer.dev@gmail.com>
2023-04-10 17:42:58 -0300

    routes home/about ok

--- my_blog/src/App.js
+++ my_blog/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 
+// React Router
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+// O navigate será usado para fazer a checagem de autenticação.
+
+//Pages
+
 function App() {
   return (
     <div className="App">
-      <h1>Kremer's blog</h1>
+      <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
