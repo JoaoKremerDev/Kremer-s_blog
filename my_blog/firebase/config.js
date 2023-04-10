8fe158d747a88842f27f276162de6c16644985ce
João Vitor Melo Kremer <joaokremer.dev@gmail.com>
2023-04-10 17:32:54 -0300

    configuração do firebase pronta

---
+++ my_blog/firebase/config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC8u_EZ8HfhB5_uAOury-fzrEgBx8rvg8M",
+  authDomain: "kremer-s-blog.firebaseapp.com",
+  projectId: "kremer-s-blog",
+  storageBucket: "kremer-s-blog.appspot.com",
+  messagingSenderId: "138315803727",
+  appId: "1:138315803727:web:fc7b806025a5d33a920e3a",
+  measurementId: "G-N5SBWE3KKC"
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+const db = getFirestore(app)
+
+export {db};
---
+++ my_blog/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Kremer's blog</h1>
+    </div>
+  );
+}
+
+export default App;
