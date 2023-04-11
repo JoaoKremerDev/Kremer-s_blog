5d0cd8bbb02fa80c1e4ec2019be0438a3c459349
João Vitor Melo Kremer <joaokremer.dev@gmail.com>
2023-04-11 15:41:01 -0300

    AuthContext created

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import "./App.css";
 
 // React Router
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// O navigate será usado para fazer a checagem de autenticação.
+
+//Pages
 import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
 import NavBar from "./Components/NavBar";
 import Footer from "./Components/Footer";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
-// O navigate será usado para fazer a checagem de autenticação.
 
-//Pages
+//context
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <div className="App">
+      <AuthProvider>
       <BrowserRouter>
       <NavBar />
         <div className="container">
@@ -27,6 +31,7 @@ function App() {
         </div>
         <Footer />
       </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
