0553a1755b0f1716e1fca1a139a6008e20319da3
João Vitor Melo Kremer <joaokremer.dev@gmail.com>
2023-04-11 10:42:21 -0300

    form styles added to global css

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,12 +1,53 @@
-import styles from './Register.module.css';
+import styles from "./Register.module.css";
 
+import { useState, useEffect } from "react";
 
 const Register = () => {
   return (
-    <div>
-        <h2>Register</h2>
+    <div className={styles.register}>
+      <h1>Cadastre-se para postar</h1>
+      <p>Crie seu usuário e compartilhe suas histórias</p>
+      <form>
+        <label>
+          <span>Nome:</span>
+          <input
+            type="text"
+            name="displayName"
+            required
+            placeholder="Nome do usuário"
+          />
+        </label>
+        <label>
+          <span>Email:</span>
+          <input
+            type="email"
+            name="displayEmail"
+            required
+            placeholder="E-mail do usuário"
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="Password"
+            required
+            placeholder="Insira a sua senha"
+          />
+        </label>
+        <label>
+          <span>Confirmação de senha:</span>
+          <input
+            type="password"
+            name="confirmPassword"
+            required
+            placeholder="Confirme a sua senha"
+          />
+        </label>
+        <button className="btn">Cadastrar</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
