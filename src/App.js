1d8c6c1f9da793727635a153585c0005da39bebc
João Vitor Melo Kremer <joaokremer.dev@gmail.com>
2023-04-22 21:27:53 -0300

    feat: project finished by now

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import "./App.css";
 
-// React Router
-// O navigate será usado para fazer a checagem de autenticação.
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 
-//hooks
+// hooks
 import { useState, useEffect } from "react";
 import { useAuthentication } from "./hooks/useAuthentication";
 
-//Pages
+// pages
 import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
-import NavBar from "./Components/NavBar";
+import Post from "./Pages/Post/Post"
+
+// components
+import Navbar from "./Components/NavBar";
 import Footer from "./Components/Footer";
+import CreatePost from "./Pages/CreatePost/CreatePost";
+import Search from "./Pages/Search/Search"
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
-import CreatePost from "./Pages/CreatePost/CreatePost";
 import Dashboard from "./Pages/Dashboard/Dashboard";
+import EditPost from "./Pages/EditPost/EditPost"
 
-//context
+// context
 import { AuthProvider } from "./context/AuthContext";
-import Search from "./Pages/Search/Search";
-import Post from "./Pages/Post/Post";
 
 function App() {
-  
   const [user, setUser] = useState(undefined);
   const { auth } = useAuthentication();
 
@@ -33,32 +33,49 @@ function App() {
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      setUser(user)
-    })
-  }, [ auth ])
-  
-  if(loadingUser) {
-    return <p>Carregando...</p>
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando...</p>;
   }
+
   return (
     <div className="App">
-      <AuthProvider value={{user}}>
-      <BrowserRouter>
-      <NavBar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/posts/:id" element={<Post />} />
-            <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
-            <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />} />
-            <Route path="/posts/create" element={user ? <CreatePost /> : <Navigate to="/login" />} />
-            <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider value={{ user }}>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route
+                path="/posts/create"
+                element={user ? <CreatePost /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/posts/edit/:id"
+                element={user ? <EditPost /> : <Navigate to="/login" />}
+              />
+              <Route path="/posts/:id" element={<Post />} />
+              <Route path="/search" element={<Search />} />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/register"
+                element={!user ? <Register /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/dashboard"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
       </AuthProvider>
     </div>
   );
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,11 +1,83 @@
-import styles from './Dashboard.module.css'
+import { useEffect, useState } from "react";
+import styles from "./Dashboard.module.css";
+
+import { Link } from "react-router-dom";
+
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useDeleteDocument } from "../../hooks/useDeleteDocument";
 
 const Dashboard = () => {
-  return (
-    <div>
-        <h1>Dashboard</h1>
-    </div>
-  )
-}
+  const { user } = useAuthValue();
+  const uid = user.uid;
 
-export default Dashboard
+  const [documents, setDocuments] = useState([]);
+
+  const { documents: posts } = useFetchDocuments("posts", null, uid);
+
+  const { deleteDocument } = useDeleteDocument("posts");
+
+  const handleDeleteDocument = async (id) => {
+    try {
+      await deleteDocument(id);
+      setDocuments(documents.filter((doc) => doc.id !== id));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Atualiza o estado local quando o componente é montado
+  useEffect(() => {
+    if (posts) {
+      setDocuments(posts);
+    }
+  }, [posts]);
+
+  return (
+    <div className={styles.dashboard}>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {documents && documents.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>  
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.post_header}>
+          <span>Título</span>
+          <span>Ações</span>
+        </div>
+      )}
+
+      {documents &&
+        documents.map((post) => (
+          <div className={styles.post_row} key={post.id}>
+            <p>{post.title}</p>
+            <div className={styles.actions}>
+              <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                Ver
+              </Link>
+              <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">
+                Editar
+              </Link>
+              <button
+                onClick={() => handleDeleteDocument(post.id)}
+                className="btn btn-outline btn-danger"
+              >
+                Excluir
+              </button>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Dashboard;
+
+
+
+
+
